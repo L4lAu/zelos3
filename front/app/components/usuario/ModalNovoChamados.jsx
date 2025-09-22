090607354afa2be5d1834fefe7bdb5bcc5c51758
L4lAu <duduzinho172007@gmail.com>
2025-09-21 21:30:52 -0300

    style: adiciona estilização das pages

--- front/app/components/usuario/ModalNovoChamados.jsx
+++ front/app/components/usuario/ModalNovoChamados.jsx
@@ -3,19 +3,19 @@ import { useState } from 'react';
 
 export default function ModalNovoChamado({ isOpen, onClose, onCreate, error }) {
   const [novoChamado, setNovoChamado] = useState({
-    patrimonio: "",
-    descricaoProblema: "",
+    numero_patrimonio: "",
+    descricao: "",
     tipo: "Manutenção"
   });
 
   const handleSubmit = () => {
     onCreate(novoChamado);
-    setNovoChamado({ patrimonio: "", descricaoProblema: "", tipo: "Manutenção" });
+    setNovoChamado({ numero_patrimonio: "", descricao: "", tipo: "Manutenção" });
   };
 
   const handleClose = () => {
     onClose();
-    setNovoChamado({ patrimonio: "", descricaoProblema: "", tipo: "Manutenção" });
+    setNovoChamado({ numero_patrimonio: "", descricao: "", tipo: "Manutenção" });
   };
 
   if (!isOpen) return null;
@@ -25,35 +25,35 @@ export default function ModalNovoChamado({ isOpen, onClose, onCreate, error }) {
       <div className="bg-[linear-gradient(10deg,#1e2128,#282c34)] p-6 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-xl font-bold text-red-600 mb-4">Novo Chamado</h2>
         
-        <div className="mb-4">
-          <label className="block text-withe mb-2">Número de Patrimônio</label>
+        <div className="mb-4 text-white">
+          <label className="block text-white mb-2">Número de Patrimônio</label>
           <input
             type="text"
             placeholder="Digite o número de patrimônio"
-            value={novoChamado.patrimonio}
-            onChange={e => setNovoChamado({...novoChamado, patrimonio: e.target.value})}
+            value={novoChamado.numero_patrimonio}
+            onChange={e => setNovoChamado({...novoChamado, numero_patrimonio: e.target.value})}
             className="w-full p-2 border border-withe rounded focus:outline-none focus:ring-2 focus:ring-red-500"
           />
         </div>
         
-        <div className="mb-4">
-          <label className="block text-withe mb-2">Tipo de Chamado</label>
+        <div className="mb-4 text-white">
+          <label className="block text-white mb-2">Tipo de Chamado</label>
           <select
             value={novoChamado.tipo}
             onChange={e => setNovoChamado({...novoChamado, tipo: e.target.value})}
             className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-500"
           >
-            <option value="Manutenção" >Manutenção</option>
-            <option value="Apoio Técnico">Apoio Técnico</option>
+            <option value="Manutenção" className=' bg-[rgba(31,31,31,0.79)] ' >Manutenção</option>
+            <option value="Apoio Técnico" className=' bg-[rgba(53,53,53,0.79)] '>Apoio Técnico</option>
           </select>
         </div>
         
-        <div className="mb-4">
-          <label className="block text-withe mb-2">Descrição do Problema *</label>
+        <div className="mb-4 text-white">
+          <label className="block text-white mb-2">Descrição do Problema *</label>
           <textarea
             placeholder="Descreva o problema em detalhes"
-            value={novoChamado.descricaoProblema}
-            onChange={e => setNovoChamado({...novoChamado, descricaoProblema: e.target.value})}
+            value={novoChamado.descricao}
+            onChange={e => setNovoChamado({...novoChamado, descricao: e.target.value})}
             className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-500 h-24"
             required
           />
